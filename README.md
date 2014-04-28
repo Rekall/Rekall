e525b195e0d397d599ea369507494277f733fb60
Guillaume Jacquemin <gj@buzzinglight.com>
2014-04-28 17:16:10 +0200

    Licence & authors update

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 About Rekall
 ============
+
+
+Build IanniX
+============
+
+All platforms
+-------------
+- Download Qt 4.8 for your plateform (http://qt-project.org/downloads) (4.7 is also compatible)
+- Download Qt Creator (http://qt-project.org/downloads)
+- Open Rekall.pro and build!
+
+Linux
+-----
+- You'll need to build Qt before building Rekall
+- Some libraries are needed for Qt building
+     - sudo apt-get install g++ libfreetype6-dev libfontconfig1-dev libfontconfig-dev libglib2.0-dev libsm-dev libgtk2.0-dev libxrender-dev libsqlite3-dev libglu1-mesa-dev libasound2-dev mesa-common-dev gtk2-engines-pixbuf libjpeg62‑dev libtiff4‑dev libpng12‑dev 
+- If opengl is slow, check Graphical Card drivers.
--- core/watcher.cpp
+++ core/watcher.cpp
@@ -1,3 +1,26 @@
+/*
+    This file is part of Rekall.
+    Copyright (C) 2013-2014
+
+    Project Manager: Clarisse Bardiot
+    Development & interactive design: Guillaume Jacquemin & Guillaume Marais (http://www.buzzinglight.com)
+
+    This file was written by Guillaume Jacquemin.
+
+    Rekall is a free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "watcher.h"
 
 Watcher::Watcher(QObject *parent) :
