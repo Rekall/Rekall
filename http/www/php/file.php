ee770163d9d4d8062c2d79eae9f77edc1e883d45
Guillaume Jacquemin <gj@buzzinglight.com>
2015-10-28 12:43:52 +0100

    Accents à la con !

--- http/www/php/file.php
+++ http/www/php/file.php
@@ -1,9 +1,17 @@
 <?php require_once("sql.php"); ?>
 <?php
 	if(isset($_GET["r"])) {
-		$filename = realpath("../file/".$_GET["r"]);
-		
-		if(startsWith($filename, realpath("../file/"))) {
+		//header('Content-Type: text/plain charset=utf-8');
+
+		$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFC));
+		if(!file_exists($filename))
+			$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFKC));
+		if(!file_exists($filename))
+			$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFD));
+		if(!file_exists($filename))
+			$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFKD));
+
+		if((file_exists($filename)) && (startsWith($filename, realpath("../file/")))) {
 			$mimeType = "";
 			$finfo = finfo_open(FILEINFO_MIME_TYPE);
 			$mimeType = finfo_file($finfo, $filename);
