e2c383bfb2739d37964d54ff90338ed29a22bcf1
Guillaume Jacquemin <gj@buzzinglight.com>
2015-10-30 20:58:56 +0100

    Bug de normalisation sous mac

--- http/www/php/file.php
+++ http/www/php/file.php
@@ -2,15 +2,18 @@
 <?php
 	if(isset($_GET["r"])) {
 		//header('Content-Type: text/plain charset=utf-8');
-
-		$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFC));
-		if(!file_exists($filename))
-			$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFKC));
-		if(!file_exists($filename))
-			$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFD));
-		if(!file_exists($filename))
-			$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFKD));
-
+		$filename = realpath("../file/".$_GET["r"]);
+		if(strpos(strtolower($_SERVER["HTTP_HOST"]), "memorekall") !== false) {
+			if(!file_exists($filename))
+				$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFC));
+			if(!file_exists($filename))
+				$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFKC));
+			if(!file_exists($filename))
+				$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFD));
+			if(!file_exists($filename))
+				$filename = realpath("../file/".Normalizer::normalize($_GET["r"], Normalizer::NFKD));
+		}
+		
 		if((file_exists($filename)) && (startsWith($filename, realpath("../file/")))) {
 			$mimeType = "";
 			$finfo = finfo_open(FILEINFO_MIME_TYPE);
@@ -18,7 +21,7 @@
 			finfo_close($finfo);
 			if($mimeType === "")
 				$mimeType = "application/octet-stream";
-			
+		
 			header("Content-Type: ".$mimeType);
 			readfile($filename);
 		}
