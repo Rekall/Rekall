49ba2cc40958d2a27e594cd5fecef7f06ce4d166
Guillaume Jacquemin <gj@buzzinglight.com>
2015-11-06 09:11:18 +0100

    Rekall v1.0

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
     //rekallApp.installTranslator(&translator);
 
     QString appName    = "Rekall ";
-    QString appVersion = "0.5.0";
+    QString appVersion = "1.0.0";
 
 #ifdef Q_OS_MAC
     appName += "Mac";
